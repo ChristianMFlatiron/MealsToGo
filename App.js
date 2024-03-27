712bb6297ff938f8dbf9069631fc4e9de496d142
ChristianMFlatiron <79779035+ChristianMFlatiron@users.noreply.github.com>
2024-03-26 19:24:28 -0700

    Restaurant info setup

--- App.js
+++ App.js
@@ -1,37 +1,12 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { StatusBar, SafeAreaView, StyleSheet, Text, View } from "react-native";
-// import { SafeAreaView } from 'react-native-safe-area-context';
+import React from "react";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.search}>Search</Text>
-        <Text style={styles.list}>list</Text>
-        <ExpoStatusBar style="auto" />
-      </SafeAreaView>
+      <RestaurantsScreen />
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-    // paddingTop: 50,
-    // paddingBottom: 50,
-    // backgroundColor: 'purple',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-  search: {
-    // flex: 1,
-    padding: 15,
-    backgroundColor: "green",
-  },
-  list: {
-    color: "green",
-    flex: 1,
-    padding: 15,
-    backgroundColor: "blue",
-  },
-});
